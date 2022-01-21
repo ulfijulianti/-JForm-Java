b13792a5d526c04e1f2cb58a322e9ef50cd66672
ulfijulianti <ulfijulianti07@gmail.com>
2022-01-21 15:33:45 +0700

    commit pertama

---
+++ KelompokApa/src/kelompokapa/KelompokApa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelompokapa;
+
+/**
+ *
+ * @author SMK N 4
+ */
+public class KelompokApa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HitungFrom hitungFrom = new HitungFrom();
+        hitungFrom.setVisible(true);
+    }
+    
+}
